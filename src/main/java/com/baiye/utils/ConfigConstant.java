078ae8281aee105dc56ac887b835d094ade53b4f
unknown <>
2016-06-16 14:11:53 +0800

    Initial commit

---
+++ src/main/java/com/baiye/utils/ConfigConstant.java
@@ -0,0 +1,14 @@
+package com.baiye.utils;
+
+/**
+ * Created by Baiye on 2016/6/16.
+ */
+public interface ConfigConstant {
+
+    String CONFIG_FILE = "baiye.properties";
+    String JDBC_DRIVE = "baiye.jdbc.driver";
+    String JDBC_URL = "baiye.jdbc.url";
+    String JDBC_USERNAME = "baiye.jdbc.username";
+    String JDBC_PASSWORD = "baiye.jdbc.password";
+
+}
