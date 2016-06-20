4526a6a7caf33ded032515acfe21a2136425af1a
Baiye <zhenghangtxdyr@gmail.com>
2016-06-20 10:25:19 +0800

    PropsUtil 完成;

--- src/main/java/com/baiye/utils/PropsUtil.java
+++ src/main/java/com/baiye/utils/PropsUtil.java
@@ -82,4 +82,70 @@ public class PropsUtil{
     {
         return props.getProperty(key,defaultValue);
     }
+
+    public static String getString(Properties props,String key,String defaultValue)
+    {
+        String value = defaultValue;
+        if(props.containsKey(key))
+            value = props.getProperty(key);
+        return value;
+    }
+
+    public Integer getInt(String key)
+    {
+        return getInt(key,null);
+    }
+
+    public Integer getInt(String key,Integer defaultValue)
+    {
+        String value = props.getProperty(key);
+        if(value != null)
+            return Integer.parseInt(value.trim());
+        return defaultValue;
+    }
+
+    public Long getLong(String key)
+    {
+        return getLong(key,null);
+    }
+
+    public Long getLong(String key,Long defaultValue)
+    {
+        String value = props.getProperty(key);
+        if(value != null)
+            return Long.parseLong(value.trim());
+        return defaultValue;
+    }
+
+    public Boolean getBoolean(String key)
+    {
+        return getBoolean(key,null);
+    }
+
+    public Boolean getBoolean(String key,Boolean defaultValue)
+    {
+        String value = props.getProperty(key);
+        if(value != null)
+        {
+            value = value.toLowerCase().trim();
+            if("true".equals(value))
+                return true;
+            else if("false".equals(value))
+                return false;
+            throw new RuntimeException("This value can not parse to Boolean : " + value);
+        }
+        return defaultValue;
+    }
+
+    public boolean containsKey(String key)
+    {
+        return props.containsKey(key);
+    }
+
+    public Properties getProperties()
+    {
+        return props;
+    }
+
+
 }
