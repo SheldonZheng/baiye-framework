6ac5eefd9b76d77e313de2de3f1f1b72669c2086
Baiye <zhenghangtxdyr@gmail.com>
2016-07-01 13:55:13 +0800

    添加Annotation支持；

---
+++ src/main/java/com/baiye/annotation/Action.java
@@ -0,0 +1,17 @@
+package com.baiye.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Baiye on 2016/7/1.
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Action {
+
+    String value();
+}
