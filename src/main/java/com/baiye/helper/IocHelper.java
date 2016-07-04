d221b6247803764da2513b65995267eed6a03769
Baiye <zhenghangtxdyr@gmail.com>
2016-07-04 11:44:16 +0800

    完成IocHelper；

---
+++ src/main/java/com/baiye/helper/IocHelper.java
@@ -0,0 +1,43 @@
+package com.baiye.helper;
+
+import com.baiye.annotation.Inject;
+import com.baiye.utils.ReflectionUtil;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by Baiye on 2016/7/4.
+ */
+public final class IocHelper {
+
+    static
+    {
+        Map<Class<?>,Object> beanMap = BeanHelper.getBeanMap();
+        if(CollectionUtils.isNotEmpty((Collection<?>) beanMap))
+        {
+            for(Map.Entry<Class<?>,Object> beanEntry : beanMap.entrySet())
+            {
+                Class<?> beanClass = beanEntry.getKey();
+                Object beanInstance = beanEntry.getValue();
+                Field[] beanFields = beanClass.getDeclaredFields();
+                if(ArrayUtils.isNotEmpty(beanFields))
+                {
+                    for(Field beanField : beanFields)
+                    {
+                        if(beanField.isAnnotationPresent(Inject.class))
+                        {
+                            Class<?> beanFieldClass = beanField.getType();
+                            Object beanFieldInstance = beanMap.get(beanFieldClass);
+                            if(beanFieldInstance != null)
+                                ReflectionUtil.setField(beanInstance,beanField,beanFieldInstance);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
