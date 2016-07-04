67431593ac0e30e229dab9f6fec17bd4147a4f4d
Baiye <zhenghangtxdyr@gmail.com>
2016-07-04 09:58:49 +0800

    完成ClassHelper；

---
+++ src/main/java/com/baiye/helper/ClassHelper.java
@@ -0,0 +1,62 @@
+package com.baiye.helper;
+
+import com.baiye.annotation.Controller;
+import com.baiye.annotation.Service;
+import com.baiye.utils.ClassUtil;
+import com.baiye.utils.ConfigConstant;
+import com.baiye.utils.PropsUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Baiye on 2016/7/1.
+ */
+public final class ClassHelper {
+    private static final Set<Class<?>> CLASS_SET;
+
+    static
+    {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    public static Set<Class<?>> getClassSet()
+    {
+        return CLASS_SET;
+    }
+
+    public static Set<Class<?>> getServiceClassSet()
+    {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+
+        for(Class<?> cls : CLASS_SET)
+        {
+            if(cls.isAnnotationPresent(Service.class))
+                classSet.add(cls);
+        }
+
+        return classSet;
+    }
+
+    public static Set<Class<?>> getControllerClassSet()
+    {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for(Class<?> cls : CLASS_SET)
+        {
+            if(cls.isAnnotationPresent(Controller.class))
+                classSet.add(cls);
+        }
+        return classSet;
+    }
+
+    public static Set<Class<?>> getBeanClassSet()
+    {
+        Set<Class<?>> beanClassSet = new HashSet<Class<?>>();
+        beanClassSet.addAll(getServiceClassSet());
+        beanClassSet.addAll(getControllerClassSet());
+        return beanClassSet;
+    }
+
+
+}
